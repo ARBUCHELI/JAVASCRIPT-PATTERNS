d195590c0cb5bc3dad420063811a0ee0d49ef4d3
ARBUCHELI <radio_seattle@hotmail.com>
2024-05-01 11:31:51 +0300

    2. creational patterns - singleton pattern

--- base/index.js
+++ base/index.js
@@ -14,6 +14,8 @@ printCalc(calc);
 */
 
 /*2. CREATIONAL PATTERNS */
+
+/* Class and constructor patterns
 class Car {
     constructor(doors, engine, color) {
         this.doors = doors;
@@ -34,8 +36,30 @@ console.log(civic);
 
 const cx5 = new SUV(4, 'V8', 'red');
 console.log(cx5);
+*/
 
+/* Singleton Patter */
 
+let instance = null;
+
+class Car {
+    constructor(doors, engine, color) {
+        if (!instance) {
+            this.doors = doors;
+            this.engine = engine;
+            this.color = color;
+            instance = this;
+        } else {
+            return instance;
+        }
+    }
+}
+
+const civic = new Car(4, 'V6', 'grey');
+const honda = new Car(2, 'V8', 'red');
+
+console.log(civic);
+console.log(honda);
 
 
 
